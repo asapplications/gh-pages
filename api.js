0192a3e31c4cc883181a1a216ead6534065c649f
asapplications <as.appdevelopment@gmail.com>
2016-04-06 18:06:45 +0200

    Added basic Scratch Files

---
+++ api.js
@@ -0,0 +1,25 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.get_question_title = function(question_number) {
+        alert(question_number);
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            [' ', 'Get Question Title No. %n', 'get_question', 1],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('FameMedia QuizMaker Scratch API', descriptor, ext);
+})({});
